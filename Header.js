a0ff3a25290e9988e3c309e31f748367719b7c44
SreyasK234 <146932745+SreyasK234@users.noreply.github.com>
2023-10-18 22:45:46 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Koduganti Tirumala Satya Sreyas</h1>
+      <div className="tile">
+        <h3>Front End Developer</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
